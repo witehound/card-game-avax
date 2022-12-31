13eeffa8021ea74d5d6a95caf7c82be8bcc5ae22
witehound <netvid00@gmail.com>
2022-12-31 16:41:02 +0100

    added a page higher order component

---
+++ src/components/PageHOC.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import { logo, heroImg } from "../assets";
+import styles from "../styles";
+
+const PageHOC = (Component) => {
+  const Page = () => {
+    const navigate = useNavigate();
+    return (
+      <div className={styles.hocContainer}>
+        <div className={styles.hocContentBox}>
+          <img
+            src={logo}
+            alt="logo"
+            className={styles.hocLogo}
+            onClick={() => {
+              navigate("/");
+            }}
+          />
+          <div className={styles.hocBodyWrapper}>
+            <Component />
+          </div>
+        </div>
+      </div>
+    );
+  };
+  return Page;
+};
+
+export default PageHOC;
