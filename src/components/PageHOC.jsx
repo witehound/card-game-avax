1f1895e265e3ca2da50778c5899b025a6a933657
witehound <netvid00@gmail.com>
2022-12-31 16:51:34 +0100

    designed the higher order component

--- src/components/PageHOC.jsx
+++ src/components/PageHOC.jsx
@@ -3,7 +3,7 @@ import { useNavigate } from "react-router-dom";
 import { logo, heroImg } from "../assets";
 import styles from "../styles";
 
-const PageHOC = (Component) => {
+const PageHOC = (Component, title, desc) => {
   const Page = () => {
     const navigate = useNavigate();
     return (
@@ -18,8 +18,20 @@ const PageHOC = (Component) => {
             }}
           />
           <div className={styles.hocBodyWrapper}>
+            <div className="flex flex-row w-full">
+              <h1 className={`flex ${styles.headText} head-text`}>{title}</h1>
+            </div>
+            <p className={`${styles.normalText} my-10`}>{desc}</p>
             <Component />
           </div>
+          <p className={styles.footerText}>Made By WingedAnibus</p>
+        </div>
+        <div className="flex flex-1">
+          <img
+            src={heroImg}
+            alt="heroImg"
+            className="w-full xl:h-full object-cover"
+          />
         </div>
       </div>
     );
--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,13 +1,15 @@
 import { PageHOC } from "../components";
 
 const Home = () => {
-  return (
-    <div>
-      <h1 className="text-5xl p-3">Avax Gods</h1>
-      <h2 className="text-3xl p-3">Web3 NFT Battle-style Card Game</h2>
-      <p className="text-xl p-3">Made with 💜 by JavaScript Mastery</p>
-    </div>
-  );
+  return <div></div>;
 };
 
-export default PageHOC(Home);
+export default PageHOC(
+  Home,
+  <>
+    Welcome to Avax Gods <br /> a Web3 NFT Card Game
+  </>,
+  <>
+    Connect your wallet to start playin <br /> The ultimate web3 NFT Card Game
+  </>
+);
